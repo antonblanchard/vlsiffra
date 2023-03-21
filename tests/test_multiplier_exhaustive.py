7a151e83f614fcbcef2e4531903698ba298e058f
Herman Schmit <schmit@google.com>
2023-03-21 01:27:33 +0000

    Extended testing for odd width multipliers
    Signed-off-by: Herman Schmit <schmit@google.com>

--- tests/test_multiplier_exhaustive.py
+++ tests/test_multiplier_exhaustive.py
@@ -15,7 +15,7 @@ class TestMultiplier(Multiplier, BoothRadix4, Dadda, NoneProcess):
     pass
 
 
-class TestCaseExhaustive(unittest.TestCase):
+class TestCaseExhaustive8(unittest.TestCase):
     def setUp(self):
         self.bits = 8
         self.dut = TestMultiplier(adder=TestAdder, bits=self.bits)
@@ -35,7 +35,31 @@ class TestCaseExhaustive(unittest.TestCase):
 
         sim = Simulator(self.dut)
         sim.add_process(bench)
-        with sim.write_vcd("multiplier_exhaustive.vcd"):
+        with sim.write_vcd("multiplier_exhaustive8.vcd"):
+            sim.run()
+
+
+class TestCaseExhaustive7(unittest.TestCase):
+    def setUp(self):
+        self.bits = 7
+        self.dut = TestMultiplier(adder=TestAdder, bits=self.bits)
+
+    def do_one_comb(self, a, b):
+        yield self.dut.a.eq(a)
+        yield self.dut.b.eq(b)
+        yield Settle()
+        res = (yield self.dut.o)
+        self.assertEqual(res, a * b)
+
+    def test_exhaustive(self):
+        def bench():
+            for a in range(int(math.pow(self.bits, 2))):
+                for b in range(int(math.pow(self.bits, 2))):
+                    yield from self.do_one_comb(a, b)
+
+        sim = Simulator(self.dut)
+        sim.add_process(bench)
+        with sim.write_vcd("multiplier_exhaustive7.vcd"):
             sim.run()
 
 
