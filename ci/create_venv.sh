8674fb5009da8573ec0884fadc4ead39fe56e83c
Tim 'mithro' Ansell <me@mith.ro>
2023-08-29 15:53:33 -0700

    Hack to get amaranth from git on newer Python versions.
    Fixes https://github.com/antonblanchard/vlsiffra/issues/26.
    
    Should be removed after Amaranth 0.4 is released.
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- ci/create_venv.sh
+++ ci/create_venv.sh
@@ -3,6 +3,13 @@
 python3 -m venv venv
 source venv/bin/activate
 python3 -c 'import sys; print("Python version:", sys.version)'
+
+# Until amaranth 0.4 is published, Pythons newer than 3.9 need to install from git.
+if [ "$(python3 -c 'import sys; print(sys.version_info[1])')" -gt 9 ]; then
+	echo "Using Amaranth from GitHub as Python >3.9 (remove after Amaranth 0.4 is released)."
+	pip3 install "git+https://github.com/amaranth-lang/amaranth.git#egg=amaranth"
+fi
+
 pip3 install .
 pip3 install unittest-parallel
 pip3 install flake8
